74f1f84f11b5c13624e839d2c62d6af94c5f6a56
Matt Davis <84292097+dattmavis@users.noreply.github.com>
2022-03-05 14:56:41 -0600

    more olark stuff
    test commit

--- static/js/olark.js
+++ static/js/olark.js
@@ -5,7 +5,14 @@ y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
 y.extend=function(i,j){y("extend",i,j)};
 y.identify=function(i){y("identify",k.i=i)};
 y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
-k=y._={s:[],t:[+new Date],c:{},l:a};
+k=y._={s:[],t:[+new Date],c:{},l:a};Bitter
 })(window,document,"static.olark.com/jsclient/loader.js");
 /* Add configuration calls below this comment */
+olark.configure('system.hb_custom_style', {
+    general: {
+      fonts: ['DejaVu Sans', 'mono'],
+      corners: '',
+      secondaryColor: ''
+    }
+  });
 olark.identify('3634-649-10-3895');
